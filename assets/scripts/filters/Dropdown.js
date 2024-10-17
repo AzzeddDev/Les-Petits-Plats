f5899f32b497dcc816a8d56f873d7ea70b097b9d
Azz Eddine SALMI <salmi.azzeddine100@gmail.com>
2024-10-17 11:32:26 +0200

    add searchInput, showItems loop, toggle classes, and functions to update selected Item and remove it

--- assets/scripts/filters/Dropdown.js
+++ assets/scripts/filters/Dropdown.js
@@ -1,12 +1,17 @@
 import {getItemsList} from "../data/getData.js"
 
 export default class Dropdown {
-    constructor(callback) {
+    constructor(type, callback) {
         this.callback = callback
         this.selectedItem = []
+        this.type = type
     }
 
-    createDropDown(viewInstance, type) {
+    /**
+     * créer le dropdown
+     * @param viewInstance
+     */
+    createDropDown(viewInstance) {
         // récup la div
         const filtersContainer = document.querySelector(".filters")
 
@@ -18,50 +23,236 @@ export default class Dropdown {
         select.className = "dropdown"
 
         // créer le dropdown avec une option
-        select.innerHTML = `<div class="type">${type}</div>`
+        select.innerHTML = `<div class="type">
+                                ${this.type}
+                                <i class="fa-solid fa-chevron-down"></i>
+                            </div>`
 
         // récupérer la liste des éléments dans un tableau
-        const itemsList = getItemsList(type)
+        const itemsList = getItemsList(this.type)
 
-        // créer un ul
-        const hiddenList = document.createElement("ul")
-        hiddenList.className = "hiddenList"
+        // créer une div cachée
+        this.hiddenList = document.createElement("div")
+        this.hiddenList.className = "hiddenList"
 
-        // boucle pour afficher en liste
-        itemsList.forEach(item => {
-            hiddenList.innerHTML += `<li>${item}</li>`
-        })
+        // créer une ul
+        const ulGroupe = document.createElement("ul")
+
+        // boucle pour afficher les items
+        this.showItems(itemsList, ulGroupe)
 
         // append le dropdown à la div
-        filtersContainer.appendChild(select).appendChild(hiddenList)
+        filtersContainer.appendChild(select).appendChild(this.hiddenList)
 
-        // ajouter un addEventListener sur la select
-        select.addEventListener("click", () => {
-            hiddenList.classList.toggle('show')
+        // toggle l'affichage du dropdown
+        const typeDiv = select.querySelector(".type")
+        typeDiv.addEventListener("click", () => {
+            this.toggle()
         })
 
         // sélectionner la div pour afficher les LI sélectionner
         const selectionDisplay = document.querySelector('.selectionDisplay')
 
-        // afficher le click du li dans la console
-        hiddenList.addEventListener("click", (event) => {
+        // créer un itemFilterSelectedDiv conteneur pour tous les items sélectionnés
+        this.itemFilterSelectedDiv = document.createElement('div')
+        this.itemFilterSelectedDiv.className = 'itemFilterSelectedDiv'
+        selectionDisplay.appendChild(this.itemFilterSelectedDiv)
+
+        // rajouter un champ de recherche
+        this.createSearchInput(ulGroupe)
+
+        // rajouter un click sur le LI
+        this.hiddenList.addEventListener("click", (event) => {
             if (event.target.tagName === 'LI') {
-                // récupérer le text event
-                const selectedText = event.target.textContent
-
-                // push l'historique
-                this.selectedItem.push(selectedText)
-
-                // console.log(selectedText)
-
-                this.callback(type, selectedText)
-
-                // rajouter l'élément sélectionner comme div sous les filtres
-                selectionDisplay.innerHTML = `<div class="selectedItem">${this.selectedItem}</div>`
-
-                // afficher dans la console
-                console.log(this.selectedItem)
+                this.selectItem(event, this.itemFilterSelectedDiv)
             }
+        })
+    }
+
+    /**
+     * Créer un champ de recherche
+     * @param ulGroupe
+     */
+    createSearchInput(ulGroupe) {
+        // ajouter un champ de recherche en haut du dropdown
+        const searchContainer = document.createElement('div')
+        const searchFlex = document.createElement("div")
+        const searchInput = document.createElement('input')
+        const clearButton = document.createElement('span')
+
+        searchContainer.className = 'searchContainer'
+        searchFlex.className = 'searchFlex'
+        searchInput.className = 'search-input'
+        searchInput.placeholder = `Rechercher ${this.type.toLowerCase()}`
+
+        // Add the clear button (X) next to the search input
+        clearButton.className = 'clear-button'
+        clearButton.textContent = 'X'  // X character
+
+        // Hide the clear button initially
+        clearButton.style.display = 'none'
+
+        // append child input sur le conteneur
+        searchContainer.appendChild(searchFlex).appendChild(searchInput)
+        searchContainer.appendChild(searchFlex).appendChild(clearButton)
+
+        // insérer la div avant les ul
+        this.hiddenList.insertBefore(searchContainer, ulGroupe)
+
+        // afficher le bouton X quand l'input n'est pas vide
+        searchInput.addEventListener('input', () => {
+            if (searchInput.value.length > 0) {
+                clearButton.style.display = 'inline'
+            } else {
+                clearButton.style.display = 'none'
+            }
+        })
+
+        // vider le champ en cliquant sur X
+        clearButton.addEventListener('click', () => {
+            searchInput.value = ''
+            clearButton.style.display = 'none'
+
+            // Show all items after clearing
+            this.hiddenList.querySelectorAll("li").forEach(item => {
+                item.style.display = "block"
+            })
+        })
+
+        this.filterItemsList()
+    }
+
+    /**
+     * Boucle pour afficher les items
+     * @param itemsList
+     * @param ulGroupe
+     */
+    showItems(itemsList, ulGroupe) {
+        // boucle pour afficher en liste
+        itemsList.forEach(item => {
+            const listItem = document.createElement("li")
+            listItem.textContent = item
+            this.hiddenList.appendChild(ulGroupe).appendChild(listItem)
+        })
+    }
+
+    /**
+     * Toggle la class 'show'
+     */
+    toggle() {
+        // toggle l'affichage
+        this.hiddenList.classList.toggle('show')
+
+        // rotation de la fleche
+        const arrow = this.hiddenList.parentElement.querySelector('.fa-chevron-down')
+        arrow.classList.toggle('rotate')
+    }
+
+    /**
+     * Sélectionner l'item
+     * @param event
+     * @param selectionDisplay
+     */
+    selectItem(event, selectionDisplay) {
+        // masqué l'élément sélectionner
+        const selectedLi = event.target
+        selectedLi.classList.add('hidden')
+
+        this.toggle()
+
+        // récupérer le text event
+        const selectedText = event.target.textContent
+
+        // vérifier si l'item a déjà été sélectionné
+        if (this.selectedItem.includes(selectedText)) {return}
+
+        // push l'historique
+        this.selectedItem.push(selectedText)
+
+        // callback
+        this.callback(this.type, selectedText)
+
+        // console.log(this.callback(this.type, selectedText))
+
+        // mettre a jour l'item sélectionner
+        this.updateItemsList(this.itemFilterSelectedDiv)
+    }
+
+    /**
+     * Mettre a jour la list des items
+     * @param itemFilterSelectedDiv
+     */
+    updateItemsList(itemFilterSelectedDiv) {
+        // vider l'affichage
+        itemFilterSelectedDiv.innerHTML = ''
+
+        // boucle pour rajouter l'item avec un addEvent pour supprimer
+        this.selectedItem.forEach((itemText, index) => {
+            const itemDiv = document.createElement('div')
+            const itemName = document.createElement('div')
+            const xMark = document.createElement('div')
+            xMark.innerHTML = `<i class="fa-solid fa-xmark"></i>`
+
+            itemDiv.className = 'selectedItem'
+            itemName.textContent = itemText
+            xMark.className = 'xMark'
+
+            // supprimer l'élément au clic
+            xMark.addEventListener('click', () => {
+                this.removeItem(itemText, itemFilterSelectedDiv)
+            })
+
+            // ajouter l'item et le xMark dans le itemFilterSelectedDiv
+            itemFilterSelectedDiv.appendChild(itemDiv).appendChild(itemName)
+            itemFilterSelectedDiv.appendChild(itemDiv).appendChild(xMark)
+        })
+    }
+
+    /**
+     * Supprimer l'item et le remettre dans la liste
+     * @param itemText
+     * @param selectionDisplay
+     */
+    removeItem(itemText, selectionDisplay) {
+        // retirer l'élément de la liste ul>li
+        this.selectedItem = this.selectedItem.filter(item => item !== itemText)
+
+        // trouver le li qui correspondant dans le dropdown
+        const allItems = Array.from(this.hiddenList.querySelectorAll('li'))
+        const itemLi = allItems.find(li => li.textContent === itemText)
+
+        // si on trouve l'élément, le réafficher dans la liste
+        if (itemLi) {
+            itemLi.classList.remove('hidden')
+        }
+
+        // mettre a jour l'item list
+        this.updateItemsList(selectionDisplay)
+    }
+
+    filterItemsList() {
+        const searchInput = document.querySelector(".search-input")
+
+        // ajouter un addEventListener sur input
+        searchInput.addEventListener("input", ()=>{
+            const query = searchInput.value.toLowerCase()
+
+            // commencer le filtering apres 3 caractères
+            if (query.length < 3) {
+                this.hiddenList.querySelectorAll("li").forEach(item => {
+                    item.style.display = "block"
+                })
+                return
+            }
+
+            this.hiddenList.querySelectorAll("li").forEach(item => {
+                const itemText = item.textContent.toLowerCase()
+                if (itemText.includes(query)) {
+                    item.style.display = "block"
+                } else {
+                    item.style.display = "none"
+                }
+            })
         })
     }
 }
